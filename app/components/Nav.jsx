f5b8d123340413e3a3e21bc733b932b8881c07f0
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-12 02:58:02 +0530

    Minor improvements

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,13 +8,15 @@ var Nav = React.createClass({
                 <div className="top-bar-left">
                     <ul className="menu">
                         <li className="menu-text">React Timer App</li>
-                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
-                        <li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+                        <li><IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
+                        <li><Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
                     </ul>
                 </div>      
                 <div className="top-bar-right">
                     <ul className="menu">
-                        <li>Created by Mayank Amencherla</li>
+                        <li className="menu-text">
+                            Created by <a href="https://github.com/mayankamencherla" target="_blank">Mayank Amencherla</a>
+                        </li>
                     </ul>
                 </div>
             </div>
