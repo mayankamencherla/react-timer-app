ecbcb339e5ecb4b211cf481727356aac4536e4f1
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-14 01:42:00 +0530

    Added a CountdownForm component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,12 +1,23 @@
 var React = require('react');
 
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+    getInitialState: function() {
+        return {
+            seconds: 0     
+        };
+    },
+    handleSetCountDown: function(seconds) {
+        this.setState({seconds});
+    },
     render: function() {
+        var {seconds} = this.state;
         return (
             <div>
-                <Clock totalSeconds={145} />
+                <Clock totalSeconds={seconds} />
+                <CountdownForm onSetCountDown={this.handleSetCountDown} />
             </div>
         );    
     }
