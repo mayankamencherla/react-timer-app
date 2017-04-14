dd8896cc0fb4d38187b93e47179684bac4ba65a8
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-14 19:53:11 +0530

    Testing countdown edge cases

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -34,5 +34,29 @@ describe('Countdown', () => {
                 done();
             }, 2001);   
         }); 
+
+        it('should pause countdown on paused status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountDown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countDownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should reset count on stopped status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountDown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countDownStatus).toBe('stopped');
+                done();
+            }, 1001);
+        });
     });
 });
